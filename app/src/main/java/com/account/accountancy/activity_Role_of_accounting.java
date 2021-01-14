2e65e419c92e0dbca78fade6fe3a3e064e5a8cf1
kishangalgotra <kk845906@gmail.com>
2021-01-14 16:03:47 +0530

    changed layout of basics

--- app/src/main/java/com/account/accountancy/activity_Role_of_accounting.java
+++ app/src/main/java/com/account/accountancy/activity_Role_of_accounting.java
@@ -30,7 +30,17 @@ public class activity_Role_of_accounting extends AppCompatActivity  implements V
         setContentView(R.layout.activity_role_of_accounting);
 
         data1 = findViewById(R.id.role_of_accounting_text);
-        sdata1 = "Accounting is often called “the language of business.” Why?\n Because it communicates so much of the information that owners, managers, and investors need to evaluate a company’s financial performance. These people are all stakeholders in the business—they’re interested in its activities because they’re affected by them. In fact, the purpose of accounting is to help stakeholders make better business decisions by providing them with financial information. Obviously, you wouldn’t try to run an organization or make investment decisions without accurate and timely financial information, and it’s the accountant who prepares this information. More importantly, accountants make sure that stakeholders understand the meaning of financial information, and they work with both individuals and organizations to help them use financial information to deal with business problems. Actually, collecting all the numbers is the easy part—today, all you have to do is start up your accounting software. The hard part is analyzing, interpreting, and communicating the information. Of course, you also have to present everything clearly while effectively interacting with people from every business discipline. In any case, we’re now ready to define accounting as the process of measuring and summarizing business activities, interpreting financial information, and communicating the results to management and other decision makers.";
+        sdata1 = "Accounting is often called “the language of business.” Why?\n" +
+                "Because it communicates so much of the information that owners, managers, and investors need to evaluate a company’s" +
+                "financial performance.\n\n" +
+                "These people are all stakeholders in the business—they’re interested in its activities because they’re affected by them.\n\n" +
+                "In fact, the purpose of accounting is to help stakeholders make better business decisions by providing them with financial information.\n\n" +
+                "Obviously, you wouldn’t try to run an organization or make investment decisions without accurate and timely financial information, and it’s the accountant who prepares this information.\n\n" +
+                "More importantly, accountants make sure that stakeholders understand the meaning of financial information, and they work with" +
+                "both individuals and organizations to help them use financial information to deal with business problems.\n\n" +
+                "Actually, collecting all the numbers is the easy part—today, all you have to do is start up your accounting software.\n\n" +
+                "The hard part is analyzing, interpreting, and communicating the information. Of course, you also have to present everything clearly while effectively interacting with people from every business discipline.\n\n" +
+                "In any case, we’re now ready to define accounting as the process of measuring and summarizing business activities, interpreting financial information, and communicating the results to management and other decision makers.";
         data1.setText(sdata1);
     }
 
