a55b4fe0f9fc9564106606d9079976e56cca5987
KISHAN GALGOTRA <kk845906@gmail.com>
2021-05-08 15:17:32 +0530

    working on Meaning of Accounting

---
+++ app/src/main/java/main/subject_screen.java
@@ -0,0 +1,16 @@
+package main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.account.accountancy.R;
+
+public class subject_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_screen);
+    }
+}
